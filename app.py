c4d456a01bde9a50c6c57007346869cea7ed5f0d
SachiVania <sachivania7@gmail.com>
2025-10-15 16:17:04 +0530

    02 : working REST API example

--- app.py
+++ app.py
@@ -1,46 +1,73 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 myapp = Flask(__name__)
 
 profiles = [
-    {
-        "id" : 1,
-        "name" : "sachi vania"
-    }, 
-    {
-        "id" : 2,
-        "name" : "nemi upadhyay"
-    }, 
-    {
-        "id" : 3,
-        "name" : "demo user"
-    }
+    {"id" : 1,"name" : "sachi vania"}, 
+    {"id" : 2,"name" : "nemi upadhyay"}, 
+    {"id" : 3,"name" : "demo user"}
 ]
 
 # get all users
 @myapp.route('/users', methods=['GET'])
 def getAllUsers():
-    pass
+    return jsonify(profiles)
 
 # get user by id
 @myapp.route('/users/<int:id>', methods=['GET'])
 def getUserById(id):
-    return 
+    for profile in profiles:
+        if profile['id'] == id: 
+            break
+    else:
+        return jsonify('profile not found')
+    
+    # another way of writing the above code
+    # profile = next((profile for profile in profiles if profile['id'] == id), None)
+    # if not profile:
+    #   return jsonify('profile not found')
+
+    return jsonify(profile)
 
 # create user
 @myapp.route('/users', methods=['POST'])
 def createUser():
-    pass
+    new_profile = request.json
+    profiles.append(new_profile)
+    return jsonify("profile added successfully")
 
 # update user
 @myapp.route('/users/<int:id>', methods=['PUT'])
 def updateUser(id):
-    pass
+    profile = next((profile for profile in profiles if profile['id'] == id), None)
+    if not profile:
+        return jsonify('profile not found. update failed.')
+
+    updated_profile = request.json
+
+    # get index of the profile to be updated.
+    index = next((i for i, profile in enumerate(profiles) if profile['id'] == id), None)
+    if index is None:
+        return jsonify('index not found. update failed.')
+
+    # update the record
+    profiles.insert(index, updated_profile)
+    return jsonify('profile updated successfully')
 
 # delete user
 @myapp.route('/users/<int:id>', methods=['DELETE'])
 def deleteUser(id):
-    pass
+    profile = next((profile for profile in profiles if profile['id'] == id), None)
+    if not profile:
+        return jsonify('profile not found. delete failed.')
+
+    # get index of the profile to be updated.
+    index = next((i for i, profile in enumerate(profiles) if profile['id'] == id), None)
+    if index is None:
+        return jsonify('index not found. delete failed.')
+
+    profiles.remove(profile)
+    return jsonify('profile deleted successfully')
 
 if __name__ == '__main__':
     myapp.run(debug=True)
