862630bf7b2fa92566ebeff352bd2b646aab863e
SachiVania <sachivania7@gmail.com>
2025-10-15 15:10:27 +0530

    02 : REST API skeleton

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
 from flask import Flask
 
-""" __name__ means name of this module ('app'.py) i.e. 'app' """
 myapp = Flask(__name__)
 
-""" 
-The route() function of the Flask class is a decorator, 
-which tells the application which URL should call 
-the associated function. 
-"""
-@myapp.route('/')
-def homepage():
-    return 'Welcome to the intial flask app!'
+profiles = [
+    {
+        "id" : 1,
+        "name" : "sachi vania"
+    }, 
+    {
+        "id" : 2,
+        "name" : "nemi upadhyay"
+    }, 
+    {
+        "id" : 3,
+        "name" : "demo user"
+    }
+]
+
+# get all users
+@myapp.route('/users')
+def getAllUsers():
+    pass
+
+# get user by id
+@myapp.route('/users/<int:id>')
+def getUserById():
+    pass
+
+# create user
+@myapp.route('/users')
+def createUser():
+    pass
+
+# update user
+@myapp.route('/users/<int:id>')
+def updateUser():
+    pass
+
+# delete user
+@myapp.route('/users/<int:id>')
+def deleteUser():
+    pass
 
 if __name__ == '__main__':
-    # run the app on the local development server
     myapp.run(debug=True)
-    """ to reload the app on code change, keep debug = True """
-
-    """
-    Be default the flask app runs on localhost and on port = 5000.
-    To change the host and/or port,
-    myapp.run(host="0.0.0.0", port=5001)
-    This is useful when we want to give a custom static ip to our flask app
-    (Assuming this ip is not already being used)
-    """
